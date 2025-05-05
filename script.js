53442cfd93e41fb9a011d575b1298978f738d333
Juraj Habala <juraj.habala@gmail.com>
2025-05-05 23:17:42 +0200

    simple try

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-const imageInput = document.querySelector("#imageInput");
-const display = document.querySelector("#display");
-
-imageInput.addEventListener("change", function () {
-    const files = this.files;
-
-    Array.from(files).forEach(file => {
-        const reader = new FileReader();
-        reader.addEventListener("load", () => {
-            const imageContainer = document.createElement("div");
-            imageContainer.style.backgroundImage = `url(${reader.result})`;
-            display.appendChild(imageContainer);
-        });
-        reader.readAsDataURL(file);
+// List of image URLs
+const imageUrls = [
+    "https://unbiased-relaxing-barnacle.ngrok-free.app/files/bethe_bloch.png",
+    "https://unbiased-relaxing-barnacle.ngrok-free.app/files/bremmsstrahlung.png"
+  ];
+  
+  // Function to display images
+  const displayImages = () => {
+    const gallery = document.getElementById("imageGallery");
+  
+    // Loop through the list of URLs
+    imageUrls.forEach(url => {
+      const imgElement = document.createElement("img");
+      imgElement.src = url;  // Set the image source to the URL
+      gallery.appendChild(imgElement);  // Append the image to the gallery
     });
-});
+  };
+  
+  // Call the function to display images
+  displayImages();
